96cec5aa6bc6b4a61906813733a4dace2852bc96
Sonet Adhikary <sonet.ad101@gmail.com>
2022-03-28 11:55:12 -0400

    Added firestore in project

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
-function addItem(event){
-      event.preventDefault();
-      let text = document.getElementById("todo-input");
-      db.collection("todo-items").add({
-            text: text.value,
-            status:"active"
-      })
-      
+import { AddDoc } from './firebase.js';
+function addItem(inputText) {
+  let text = document.getElementById('todo-input');
 
+  AddDoc('todo-items', {
+    text: inputText,
+    status: 'active',
+  });
 }
+
+function test(e) {
+  e.preventDefault();
+}
+
+const todo = document.getElementById('todo-input');
+
+const form = document.getElementById('form');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  addItem(todo.value);
+  todo.value = '';
+});
